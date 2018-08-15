6b976661e000946b2f9dfe5ea9eba6fcdc3a8bdf
Plawan Rath <plawanrath@gmail.com>
2018-08-14 23:13:03 -0400

    merge k sorted lists

--- leetcode/MergeKSorted.js
+++ leetcode/MergeKSorted.js
@@ -1,101 +1,32 @@
-
-//Definition for singly-linked list.
-function ListNode(val) {
-    this.val = val;
-    this.next = null;
-}
-
-/**
- * @param {ListNode[]} lists
- * @return {ListNode}
- */
-function heap() {
-    this.value = [];
-}
-
-heap.prototype.swap = function(i, j) {
-    var temp = this.value[i];
-    this.value[i] = this.value[j];
-    this.value[j] = temp;
-}
-
-function getParentIndex(index) {
-    return Math.floor((index-1)/2);
-}
-
-function getLeftChildIndex(index) {
-    return (2*index)+1;
-}
-
-function getRightChildIndex(index) {
-    return (2*index)+2;
-}
-
-heap.prototype.add = function(item) {
-    this.value.push(item);
-    if(this.value.length === 1) {
-        return;
-    }
-    this.heapifyUp();
-}
-
-heap.prototype.remove = function() {
-    if(this.value.length === 0) {
-        return NaN;
-    }
-    if(this.value.length === 1) {
-        return this.value.pop();
-    }
-    this.swap(0, this.value.length-1);
-    var ret = this.value.pop();
-    this.heapifyDown();
-    return ret;
-}
-
-heap.prototype.length = function() {
-    return this.value.length;    
-}
-
-heap.prototype.heapifyUp = function() {
-    var index = this.value.length-1;
-    var parent = getParentIndex(index);
-    while(index >= 0 && this.value[parent] > this.value[index]) {
-        this.swap(index, parent);
-        index = parent;
-        parent = getParentIndex(index);
-    }
-}
-
-heap.prototype.heapifyDown = function() {
-    var index = 0;
-    var lchildInd = getLeftChildIndex(index);
-    var rchildInd = getRightChildIndex(index);
-    while(index < this.value.length && (this.value[lchildInd] < this.value[index] || this.value[rchildInd] < this.value[index])) {
-        if(this.value[lchildInd] <= this.value[rchildInd]) {
-            this.swap(index, lchildInd);
-            index = lchildInd;
-        } else {
-            this.swap(index, rchildInd);
-            index = rchildInd;
-        }
-        lchildInd = getLeftChildIndex(index);
-        rchildInd = getRightChildIndex(index);
-    }
-}
-
 var mergeKLists = function(lists) {
-    let pq = new heap();
-    for(let i=0;i<lists.length;i++) {
-        while(lists[i] != null) {
-            pq.add(lists[i].val);
-            lists[i] = lists[i].next;
-        }
+    return mergeKListsRec(lists, 0, lists.length-1)    
+}
+
+var mergeKListsRec = function(lists, start, end) {
+    if(lists == null || lists.length <= 0) {
+        return lists;
     }
-    let res = new ListNode(null);
-    let head = res;
-    while(pq.length >= 0) {
-        res.next = new LisNode(pq.remove());
-        res = res.next;
+    if(start == end) {
+        return lists[start];
     }
-    return head.next;    
+    let mid = Math.floor((start+end)/2);
+    let left = mergeKListsRec(lists, start, mid);
+    let right = mergeKListsRec(lists, mid+1, end);
+    return merge2(left, right);
 };
+
+function merge2(list1, list2) {
+    if(list1 == null) {
+        return list2;
+    }
+    if(list2 == null) {
+        return list1;
+    }
+    if(list1.val < list2.val) {
+        list1.next = merge2(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = merge2(list1, list2.next);
+        return list2;
+    }
+}
