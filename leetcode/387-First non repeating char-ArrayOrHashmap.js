b2c1f34805e9a80288bf9557d421119be9b5d5db
Plawan Rath <plawanrath@gmail.com>
2018-09-09 11:14:46 -0400

    updates

--- leetcode/387-First non repeating char-ArrayOrHashmap.js
+++ leetcode/387-First non repeating char-ArrayOrHashmap.js
@@ -1,29 +1,19 @@
 function nonRepeating(s) {
-    var array = new Array(26).fill(0);
-    for(var i=0;i<s.length;i++) {
-        var index = s.charCodeAt(i) - 97;
-        array[index]++;
-    }
-    charArr = {};
-    for(var i=0;i<array.length;i++) {
-        if(array[i] === 1) {
-            var chr = String.fromCharCode(97 + i);
-            charArr[chr] = s.indexOf(chr); 
+    let map = new Map();
+    //populate map of count of characters
+    for(let i=0;i<s.length;i++) {
+        if(!map[s[i]]) {
+            map[s[i]] = 1;
+        } else {
+            map[s[i]]++;
         }
     }
-    if(Object.keys(charArr).length === 0) {
-        return -1; 
-    }
-    var min = undefined;
-    for(var key in charArr) {
-        if(min === undefined) {
-            min = charArr[key];
-        }
-        if(charArr[key] <= min) {
-            min = charArr[key];
+    for(let i=0;i<s.length;i++) {
+        if(map[s[i]] == 1) {
+            return i;
         }
     }
-    return min;
+    return null;
 }
 
 console.log(nonRepeating("leetcode"));
