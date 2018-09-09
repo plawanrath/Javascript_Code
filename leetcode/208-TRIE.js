8a0c0fe28367efda583f9f306d1513e6b393dd9f
Plawan Rath <plawanrath@gmail.com>
2018-09-09 11:10:10 -0400

    updates

--- leetcode/208-TRIE.js
+++ leetcode/208-TRIE.js
@@ -1,68 +1,59 @@
 /**
  * Initialize your data structure here.
  */
-var Trie = function() {
-    this.value = "";
-    this.array = new Array(26).fill(null);
-};
-
-/**
- * Inserts a word into the trie. 
- * @param {string} word
- * @return {void}
- */
-Trie.prototype.insert = function(word) {
-    word = word.toLowerCase();
-    trnode = this;
-    for(var i=0;i<word.length;i++) {
-        var letter = word.charCodeAt(i) - 97;
-        if(trnode.array[letter] === null) {
-            var lt = new Trie();
-            trnode.array[letter] = lt;            
-        }
-        trnode = trnode.array[letter];
+class Trie {
+    constructor() {
+        this.nodes = new Map();
+        this.isWord = false;
     }
-    trnode.value = word;
-};
 
-/**
- * Returns if the word is in the trie. 
- * @param {string} word
- * @return {boolean}
- */
-Trie.prototype.search = function(word) {
-    word = word.toLowerCase();
-    var trnode = this;
-    for(var i=0;i<word.length;i++) {
-        var index = word.charCodeAt(i) - 97;
-        if(trnode.array[index] === null) {
-            return false;
+    /**
+     * Inserts a word into the trie
+     * @param {string} word
+     * @return {void}
+     */
+    insert(word) {
+        let trnode = this.nodes;
+        for(let i=0;i<word.length;i++) {
+            if(!trnode[word[i]]) {
+                trnode[word[i]] = new Trie();
+            }
+            trnode = trnode[word[i]];
         }
-        trnode = trnode.array[index];
+        trnode.isWord = true;
     }
-    if(trnode.value === word) {
+
+    /** Returns if the word is on the Trie
+     *  @param {string} word
+     *  @return {boolean}
+     */
+    search(word) {
+        let trnode = this.nodes;
+        for(let i=0;i<word.length;i++) {
+            if(!trnode[word[i]]) {
+                return false;
+            }
+            trnode = trnode[word[i]];
+        }
+        return trnode.isWord;
+    }
+
+    /**
+     * Returns if there is any word in the trie that starts with the given prefix.
+     * @param {string} prefix
+     * @param {boolean}
+     */
+    startsWith(prefix) {
+        let trnode = this.nodes;
+        for(let i=0;i<prefix.length;i++) {
+            if(!trnode[prefix[i]]) {
+                return false;
+            }
+            trnode = trnode[prefix[i]];
+        }
         return true;
     }
-    return false;
-};
-
-/**
- * Returns if there is any word in the trie that starts with the given prefix. 
- * @param {string} prefix
- * @return {boolean}
- */
-Trie.prototype.startsWith = function(prefix) {
-    prefix = prefix.toLowerCase();
-    var trnode = this;
-    for(var i=0;i<prefix.length;i++) {
-        var index = prefix.charCodeAt(i) - 97;
-        if(trnode.array[index] === null) {
-            return false;
-        }
-        trnode = trnode.array[index];
-    }
-    return true;
-};
+}
 
 /** 
  * Your Trie object will be instantiated and called as such:
